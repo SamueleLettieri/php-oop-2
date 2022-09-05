26e5d508053c12a23aaccf0f6586f59e5a60a0dd
SamueleLettieri <samuele2073@gmail.com>
2022-09-05 19:04:34 +0200

    fixed mistake in creditCard.php

--- classes/creditCard.php
+++ classes/creditCard.php
@@ -16,7 +16,7 @@ class creditCard extends payment{
     
     public function setcreditCardExpired($creditCardExpired){
         if($creditCardExpired == true){
-        $this->creditCardExpired = null;
+        $this->creditCard = null;
         }
     }
 
